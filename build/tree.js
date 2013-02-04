0b7c4d72c9b06cfbe580eb784149ff9318d356fc
Titouan Galopin <galopintitouan@gmail.com>
2013-02-04 16:55:47 +0100

    Add API doc

---
+++ build/tree.js
@@ -0,0 +1,6 @@
+var tree = [["[Global Namespace]",,"",
+                [["Yampee_Translator_Array","Yampee_Translator_Array.html","",
+                [        ]],["Yampee_Translator_Gettext","Yampee_Translator_Gettext.html","",
+                [        ]],["Yampee_Translator_Interface","Yampee_Translator_Interface.html","",
+                [        ]]        ]]        ]
+
